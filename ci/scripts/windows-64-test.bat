b658e571d1637010c3ecb72a4ea0f96646ec5337
David Varvel and Derek Richard <drichard@pivotallabs.com>
2014-09-10 11:26:17 -0700

    Try a different gopath

--- ci/scripts/windows-64-test.bat
+++ ci/scripts/windows-64-test.bat
@@ -8,7 +8,7 @@ go-bindata -pkg resources -ignore ".go" -o cf/resources/i18n_resources.go cf/i18
 powershell -command set-executionpolicy remotesigned
 powershell .\bin\replace-sha.ps1
 
-SET GOPATH=%CD%;%CD%\Godeps\_workspace;c:\Users\Administrator\go
+SET GOPATH=C:\Users\pivotal\Go-Agent\pipelines\Windows-Testing;%CD%\Godeps\_workspace;c:\Users\Administrator\go
 c:\Go\bin\go build -v -o cf-windows-amd64.exe ./main
 c:\Go\bin\go test -i ./cf/... ./generic/... ./testhelpers/...
 c:\Go\bin\go test -cover -v ./cf/... ./generic/... ./testhelpers/...
